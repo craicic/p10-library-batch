38f68ade8bdd1f4c6d81b2cb64895891a84d5f9c
xxjokerx <rouzic.jeremie@gmail.com>
2019-04-08 13:14:24 +0200

    Some configurations have been externalised in application.properties

--- src/main/java/com/gg/proj/config/ConsumerConfiguration.java
+++ src/main/java/com/gg/proj/config/ConsumerConfiguration.java
@@ -2,12 +2,21 @@ package com.gg.proj.config;
 
 import com.gg.proj.consumer.ProfileConnector;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 @Configuration
+@EnableConfigurationProperties
 public class ConsumerConfiguration {
+
+    private ConsumerProperties consumerProperties;
+
+    @Autowired
+    public ConsumerConfiguration(ConsumerProperties consumerProperties) {
+        this.consumerProperties = consumerProperties;
+    }
 
     @Bean
     public Jaxb2Marshaller profileMarshaller() {
@@ -21,7 +30,7 @@ public class ConsumerConfiguration {
     @Bean
     public ProfileConnector profileConnector(@Autowired Jaxb2Marshaller profileMarshaller) {
         ProfileConnector client = new ProfileConnector();
-        client.setDefaultUri("http://localhost:8080/ws");
+        client.setDefaultUri(consumerProperties.getUri());
         client.setMarshaller(profileMarshaller);
         client.setUnmarshaller(profileMarshaller);
         return client;
---
+++ src/main/java/com/gg/proj/config/ConsumerProperties.java
@@ -0,0 +1,24 @@
+package com.gg.proj.config;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties("batch.webservice")
+@Component
+public class ConsumerProperties {
+
+    /**
+     *
+     * URI of the SOAP web service that this application uses to reach the endpoint.
+     */
+    private String uri = "http://localhost:8080/ws";
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+}
